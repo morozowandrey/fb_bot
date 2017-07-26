3af62c1e7c3d925dc3320c0f97bafa02d05d9535
Andrew Morozow <andrey940@gmail.com>
2017-07-26 18:44:43 +0300

    comment script done

--- fbLogin.js
+++ fbLogin.js
@@ -7,7 +7,7 @@ var pageResponses = {};
 
 page.settings.userAgent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.98 Safari/537.36';
 page.settings.javascriptEnabled = true;
-page.settings.loadImages = false;
+page.settings.loadImages = true;
 phantom.cookiesEnabled = true;
 phantom.javascriptEnabled = true;
 
@@ -20,7 +20,10 @@ if(fs.isFile(CookieJar))
         phantom.addCookie(x);
     });
 
+
 ///////////////////////////////////////////////////////////////////////////////////////////////
+/* LOGIC */
+
 var login = function (callback) {
     page.open("https://m.facebook.com/", function(status) {
         if ( status === "success" ) {
@@ -40,39 +43,60 @@ var login = function (callback) {
 };
 
 var targetPage = function (callback) {
-    page.open("https://m.facebook.com/story.php?story_fbid=103003277024225&id=100019436589542&fs=0", function (status) {
+    page.open("https://m.facebook.com/story.php?story_fbid=103003277024225&id=100019436589542", function (status) {
         if ( status === "success" ) {
+            var links = page.evaluate(function() {
+                // creating links array
+                var linksArr= [];
+                document.querySelectorAll('.story_body_container div[data-sigil="m-feed-voice-subtitle"] > a:first-child').forEach(function (el, i) {
+                    linksArr[i]=el.getAttribute('href');
+                });
+                // to do:
+                // *promise
+                // *for each проверка интераций (счетчик)
+                // *setTimeout
+            });
             window.setTimeout(function () {
-                console.log("checkins page achieved");
-                page.render('checkins_page.jpg');
+                return linksArr;
+            }, 1000);
+
+
+            // parse links from lincksArr, open pages with this links and post comment in it with terminal
+            window.setTimeout(function () {
+                for (var i = 0; i <  links.length; i++){
+                    page.open(links[i], function (status) {
+                        if ( status === "success" ) {
+                            if (document.querySelector('input[name="comment_text"]').value !='comment text'){
+                                var el = document.querySelector(el);
+                                el.value='comment text';
+                                MAjaxify.form(event,document.querySelector('form'),"async_composer","cache",null,false);
+                            }
+                        }
+                    });
+                }
+
+                console.log("comments done");
+                page.render('comment.jpg');
                 phantom.exit();
-                parser(page);
-            }, 5000);
+            }, 3000);
         }
     });
 };
 
-var parser = function (page){
-    var test= page.evaluate(function() {
+function comment(el) {
+    var el = document.querySelector(el);
+        el.value='comment';
+    MAjaxify.form(event,document.querySelector('form'),"async_composer","cache",null,false);
+}
 
-        function comment(el) {
-            el = document.querySelector(el);
-            el.value='comment';
-
-            MAjaxify.form(new MouseEvent('click', {
-                'view': window,
-                'bubbles': true,
-                'cancelable': true
-            }),document.querySelector('form'),"async_composer","cache",null,false);
-        }
-        comment('[name="comment_text"]');
-
+function simulateClick (el) {
+    var click = new MouseEvent('click', {
+        'view': window,
+        'bubbles': true,
+        'cancelable': true
     });
-    window.setTimeout(function () {
-        console.log(test);
-        console.log("all done");
-        page.render('comment done.jpg');
-        phantom.exit();
-    }, 5000);
-};
+    var clickTarget = document.querySelector(el);
+    var cancelled = !clickTarget.detachEvent(event);
+}
+
 login(targetPage);
